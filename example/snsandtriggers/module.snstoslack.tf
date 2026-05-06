b10637da9d8d56a6755ffa5d0e1c0c31ab7c7bbd
James Woolfenden <james.woolfenden@gmail.com>
2026-05-06 10:07:17 +0100

    refresh

--- example/snsandtriggers/module.snstoslack.tf
+++
@@ -1,8 +0,0 @@
-module "snstoslack" {
-  source         = "../../"
-  arns           = var.arns
-  common_tags    = var.common_tags
-  rules          = var.rules
-  SLACK_CHANNEL  = var.SLACK_CHANNEL
-  SLACK_HOOK_URL = var.SLACK_HOOK_URL
-}
